f00846a36c688a44f871daff78c42ad79497e0c1
Simon Rice <im@simonrice.com>
2017-06-10 19:22:40 +0100

    Various corrections

--- lib/fastlane/plugin/acknowledgements/actions/acknowledgements_action.rb
+++ lib/fastlane/plugin/acknowledgements/actions/acknowledgements_action.rb
@@ -1,93 +1,96 @@
 require 'CFPropertyList'
+
 require 'redcarpet'
+require 'redcarpet/render_strip'
 
 module Fastlane
   module Actions
     class AcknowledgementsAction < Action
 
-      ACKNOWLEDGEMENT_GENERATOR_LICENSE = '"THE BEER-WARE LICENSE" (Revision 42):' +
+      ACKNOWLEDGEMENT_GENERATOR_LICENSE = '"THE BEER-WARE LICENSE" (Revision 42):' \
         '
-        ' +
-        '<http://www.knage.net> & <https://simonrice.com> wrote this file. As long as you retain this notice you ' +
-        'can do whatever you want with this stuff. If we meet some day, and you think ' +
+        ' \
+        '<http://www.knage.net> & <https://simonrice.com> wrote this file. As long as you retain this notice you ' \
+        'can do whatever you want with this stuff. If we meet some day, and you think ' \
         'this stuff is worth it, you can buy me a beer in return Christophe Vallinas Knage & Simon Rice.'
 
-        def self.create_acknowledgement(name, content, settings_bundle)
-          # Create a arbitrary data structure of basic data types
-          acknowledgement = {
-            'StringsTable' => 'ThirdPartyLicenses',
-            'PreferenceSpecifiers' => [{
-              'Type' => 'PSGroupSpecifier',
-              'FooterText' => content
-              }]
-          }
+      def self.create_acknowledgement(name, content, settings_bundle)
+        # Create a arbitrary data structure of basic data types
+        acknowledgement = {
+          'StringsTable' => 'ThirdPartyLicenses',
+          'PreferenceSpecifiers' => [{
+            'Type' => 'PSGroupSpecifier',
+            'FooterText' => content
+          }]
+        }
 
-          # Generate name for acknowledgement plist
-          acknowledgementName = "Acknowledgement-" + name
+        # Generate name for acknowledgement plist
+        acknowledgement_name = "Acknowledgement-#{name}"
 
-          # Generate item for acknowledgement list
-          acknowledgementListItem = {
-                  'Type' => 'PSChildPaneSpecifier',
-                  'Title' => name,
-                  'File' => acknowledgementName
-              }
+        # Generate item for acknowledgement list
+        acknowledgement_list_item = {
+          'Type' => 'PSChildPaneSpecifier',
+          'Title' => name,
+          'File' => acknowledgement_name
+        }
 
-          # Create plist file
-          plist = CFPropertyList::List.new
-          plist.value = CFPropertyList.guess(acknowledgement)
-          plist.save(settings_bundle + "/" + acknowledgementName + ".plist", CFPropertyList::List::FORMAT_XML)
+        # Create plist file
+        plist = CFPropertyList::List.new
+        plist.value = CFPropertyList.guess(acknowledgement)
+        plist.save("#{settings_bundle}/#{acknowledgement_name}.plist", CFPropertyList::List::FORMAT_XML)
 
-          # return
-          acknowledgementListItem
+        # return
+        acknowledgement_list_item
       end
 
-      def self.save_acknowledgement_list(acknowledgementListItems, settings_bundle)
-          # Create a arbitrary data structure of basic data types
-          acknowledgementList = {
-            'StringsTable' => 'ThirdPartyLicenses',
-            'PreferenceSpecifiers' => acknowledgementListItems
-          }
+      def self.save_acknowledgement_list(acknowledgement_list_items, settings_bundle)
+        # Create a arbitrary data structure of basic data types
+        acknowledgement_list = {
+          'StringsTable' => 'ThirdPartyLicenses',
+          'PreferenceSpecifiers' => acknowledgement_list_items
+        }
 
-          # Create plist file
-          plist = CFPropertyList::List.new
-          plist.value = CFPropertyList.guess(acknowledgementList)
-          plist.save(settings_bundle + "/Acknowledgements.plist", CFPropertyList::List::FORMAT_XML)
+        # Create plist file
+        plist = CFPropertyList::List.new
+        plist.value = CFPropertyList.guess(acknowledgement_list)
+        plist.save("#{settings_bundle}/Acknowledgements.plist", CFPropertyList::List::FORMAT_XML)
       end
 
       def self.run(params)
         # UI.message "Parameter API Token: #{params[:api_token]}"
 
         settings_bundle = params[:settings_bundle]
-        acknowledgement_list = Array.new
+        acknowledgement_list = []
         Dir.glob("#{params[:license_path]}/*#{params[:license_extension]}") do |file|
-            UI.message "Parsing: #{file.split('/').last}..."
+          UI.message "Parsing: #{file.split('/').last}..."
 
-            # Extract name
-            license_file_name = File.basename(file)
-            project_name = license_file_name.split('.').first
+          # Extract name
+          license_file_name = File.basename(file)
+          project_name = license_file_name.split('.').first
 
-            # Extract license
-            license_file = File.open(file)
-            project_license = license_file.read
+          # Extract license
+          license_file = File.open(file)
+          project_license = license_file.read
+          parser = Redcarpet::Markdown.new(Redcarpet::Render::StripDown, space_after_headers: true)
+          project_license = parser.render(project_license)
 
-            # Create Acknowledgement- plist
-            parser = Redcarpet::Markdown.new(renderer, extensions = {})
-            acknowledgement_item = create_acknowledgement(project_name, project_license, settings_bundle)
-            license_file.close
-            acknowledgement_list.push(acknowledgement_item)
+          # Create Acknowledgement- plist
+          acknowledgement_item = create_acknowledgement(project_name, project_license, settings_bundle)
+          license_file.close
+          acknowledgement_list.push(acknowledgement_item)
         end
 
         # Push AcknowledgementGenerator License
-        acknowledgement_generator_license = create_acknowledgement("AcknowledgementGenerator (Fastlane Edition)", ACKNOWLEDGEMENT_GENERATOR_LICENSE, settings_bundle)
+        acknowledgement_generator_license = create_acknowledgement('AcknowledgementGenerator (Fastlane Edition)', ACKNOWLEDGEMENT_GENERATOR_LICENSE, settings_bundle)
         acknowledgement_list.push(acknowledgement_generator_license)
-        sorted_acknowledgements = acknowledgement_list.sort_by { |item| item["Title"].downcase }
+        sorted_acknowledgements = acknowledgement_list.sort_by { |item| item['Title'].downcase }
 
         # Create Acknowledgements plist
         UI.message "Adding acknowledgements to #{settings_bundle}..."
         save_acknowledgement_list(sorted_acknowledgements, settings_bundle)
 
         # All finished
-        UI.success "Acknowledgement Generator Finished"
+        UI.success 'Acknowledgement Generator Finished'
       end
 
       #####################################################
@@ -95,13 +98,7 @@ module Fastlane
       #####################################################
 
       def self.description
-        "Use Fastlane to give credit where it's rightfully due"
-      end
-
-      def self.details
-        # Optional:
-        # this is your chance to provide a more detailed description of this action
-        "You can use this action to do cool things..."
+        "Use Fastlane to give credit where it's rightfully due."
       end
 
       def self.available_options
@@ -110,28 +107,28 @@ module Fastlane
                                        env_name: "FL_ACKNOWLEDGEMENTS_SETTINGS_BUNDLE",
                                        description: "Location of the settings bundle file",
                                        verify_block: proc do |value|
-                                          UI.user_error!("No Settings Bundle path for AcknowledgementsAction given, pass using `settings_bundle: 'path/to/settings.bundle'`") unless (value and not value.empty?)
-                                          UI.user_error!("Couldn't find file at path '#{value}'") unless File.exist?(value)
+                                         UI.user_error!("No Settings Bundle path for AcknowledgementsAction given, pass using `settings_bundle: 'path/to/settings.bundle'`") unless value && !value.empty?
+                                         UI.user_error!("Couldn't find file at path '#{value}'") unless File.exist?(value)
                                        end),
 
           FastlaneCore::ConfigItem.new(key: :license_path,
                                        env_name: "FL_ACKNOWLEDGEMENTS_LICENSE_PATH",
                                        description: "Path containing the license files",
                                        verify_block: proc do |value|
-                                          UI.user_error!("No License path for AcknowledgementsAction given, pass using `license_path: 'path/to/licenses/'`") unless (value and not value.empty?)
-                                          UI.user_error!("Couldn't find file at path '#{value}'") unless File.exist?(value)
+                                         UI.user_error!("No License path for AcknowledgementsAction given, pass using `license_path: 'path/to/licenses/'`") unless value && !value.empty?
+                                         UI.user_error!("Couldn't find file at path '#{value}'") unless File.exist?(value)
                                        end),
 
           FastlaneCore::ConfigItem.new(key: :license_path,
                                        env_name: "FL_ACKNOWLEDGEMENTS_LICENSE_EXTENSION",
                                        description: "File extension for license files.  Defaults to '.license'.",
-                                       default_value: '.license'),
+                                       default_value: '.license')
         ]
       end
 
       def self.authors
         # So no one will ever forget your contribution to fastlane :) You are awesome btw!
-        ["@_simonrice", "@cvknage"]
+        ['@_simonrice', '@cvknage']
       end
 
       def self.is_supported?(platform)
--- spec/acknowledgements_spec.rb
+++ spec/acknowledgements_spec.rb
@@ -2,11 +2,11 @@ describe Fastlane::Actions::AcknowledgementsAction do
   describe '#run' do
     it 'Creates a set of acknowledgements files' do
       allow(Fastlane::UI).to receive(:success)
-      expect(Fastlane::UI).to receive(:success).with("Acknowledgement Generator Finished")
+      expect(Fastlane::UI).to receive(:success).with('Acknowledgement Generator Finished')
 
       Fastlane::Actions::AcknowledgementsAction.run(
-        settings_bundle: "example-project/settings.bundle",
-        license_path: "example-project/licenses",
+        settings_bundle: 'example-project/settings.bundle',
+        license_path: 'example-project/licenses'
       )
     end
   end
